242cf57f60c9ab4086476d840e9c270a0457f30d
Nikhil Kumar Yadav <nikhilyadavindigo@gmail.com>
2020-10-07 16:53:11 +0530

    [Nikhil] Added_UC1

--- Program.cs
+++ Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Text.RegularExpressions;
 
-namespace UserRegistration_git
+namespace Regex_UC
 {
+    class Pattern
+    {
+        public static string REGEX_ENTRY = "^[A-Z][a-zA-Z]{2,}$";
+        public bool validateFirstName(string firstName)
+        {
+            return Regex.IsMatch(firstName, REGEX_ENTRY);
+        }
+    }
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Pattern pattern = new Pattern();
+            Console.WriteLine("Enter First Name:");
+            if (pattern.validateFirstName(Console.ReadLine()))
+            {
+                Console.WriteLine("First Name is Valid");
+            }
+            else
+                Console.WriteLine("First Name is Invalid");
         }
     }
 }
